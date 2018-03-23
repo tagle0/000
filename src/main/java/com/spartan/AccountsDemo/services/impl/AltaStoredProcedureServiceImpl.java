a73cf0a93060b532fd50f4366c21aa566730b960
tagle0 <alejandro.tagle.0@gmail.com>
2018-03-23 16:31:14 -0600

    Added Test for Stored procedure

--- src/main/java/com/spartan/AccountsDemo/services/impl/AltaStoredProcedureServiceImpl.java
+++ src/main/java/com/spartan/AccountsDemo/services/impl/AltaStoredProcedureServiceImpl.java
@@ -32,24 +32,27 @@ public class AltaStoredProcedureServiceImpl implements AltaStoredProcedureServic
     public AltaStoredProcedureResponse callAlta(String nombre, String telefono, String direccion, String correo, Double saldo_in, String ejecutivo) {
         AltaStoredProcedure a = new AltaStoredProcedure(dataSource);
         AltaStoredProcedureResponse response = new AltaStoredProcedureResponse();
-        try {
-              CallableStatement cs = dataSource.getConnection().prepareCall("call alta(?,?,?,?,?,?,?,?,?)");
-            cs.setString(1, nombre);
-            cs.setString(2, telefono);
-            cs.setString(3, direccion);
-            cs.setString(4, correo);
-            cs.setDouble(5, saldo_in);
-            cs.setString(6, ejecutivo);
-            cs.registerOutParameter(7, java.sql.Types.INTEGER);
-            cs.registerOutParameter(8, java.sql.Types.INTEGER);
-            cs.registerOutParameter(9, java.sql.Types.DECIMAL);
-            cs.executeUpdate();
-            response.setClienteId(cs.getInt(7));
-            response.setCuentaId(cs.getInt(8));
-            response.setSaldo(cs.getDouble(9));
-        } catch (SQLException ex) {
-            Logger.getLogger(AltaStoredProcedureServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            try {
+                  CallableStatement cs = dataSource.getConnection().prepareCall("{call alta(?,?,?,?,?,?,?,?,?)}");
+                cs.setString(1, nombre);
+                cs.setString(2, telefono);
+                cs.setString(3, direccion);
+                cs.setString(4, correo);
+                cs.setDouble(5, saldo_in);
+                cs.setString(6, ejecutivo);
+
+                cs.registerOutParameter(7, java.sql.Types.INTEGER);
+                cs.registerOutParameter(8, java.sql.Types.INTEGER);
+                cs.registerOutParameter(9, java.sql.Types.DECIMAL);
+
+                cs.executeUpdate();
+
+                response.setClienteId(cs.getInt(7));
+                response.setCuentaId(cs.getInt(8));
+                response.setSaldo(cs.getDouble(9));
+            } catch (SQLException ex) {
+                Logger.getLogger(AltaStoredProcedureServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
         return response;
     }
 
--- src/test/java/com/spartan/AccountsDemo/AccountsDemoApplicationTests.java
+++ src/test/java/com/spartan/AccountsDemo/AccountsDemoApplicationTests.java
@@ -1,14 +1,16 @@
 package com.spartan.AccountsDemo;
 
 import com.spartan.AccountsDemo.dao.pojo.AltaStoredProcedureResponse;
+import com.spartan.AccountsDemo.pojo.Cliente;
 import com.spartan.AccountsDemo.repositories.ClienteCrudRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.spartan.AccountsDemo.services.AltaStoredProcedureServiceI;
+import java.util.Optional;
+import org.junit.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,7 +18,7 @@ public class AccountsDemoApplicationTests {
 
     @Autowired
     AltaStoredProcedureServiceI altaStoredProcedureService;
-    
+
     @Autowired
     ClienteCrudRepository clienteCrudRepository;
 
@@ -32,7 +34,17 @@ public class AccountsDemoApplicationTests {
         String correo = "mail@domain.com";
         Double saldo = 999.99d;
         String ejecutivo = "Anonymous test";
-        AltaStoredProcedureResponse altaStoredProcedureResponse = altaStoredProcedureService.callAlta(nombre, telefono, direccion, correo, Double.NaN, ejecutivo);
+        AltaStoredProcedureResponse altaStoredProcedureResponse = altaStoredProcedureService.callAlta(nombre, telefono, direccion, correo, saldo, ejecutivo);
+        Optional<Cliente> optionalClient = clienteCrudRepository.findById(altaStoredProcedureResponse.getClienteId());
+        Assert.assertTrue("Client " + altaStoredProcedureResponse.getClienteId() + " was not recovered from DB", optionalClient.isPresent());
+        if (optionalClient.isPresent()) {
+            Cliente cliente = optionalClient.get();
+            Assert.assertEquals("Nombre was not recovered correctly (" + cliente.getNombre() + ")", nombre, cliente.getNombre());
+            Assert.assertEquals("Nombre was not recovered correctly (" + cliente.getCorreo()+ ")", correo, cliente.getCorreo());
+            Assert.assertEquals("Nombre was not recovered correctly (" + cliente.getTelefono()+ ")", telefono, cliente.getTelefono());
+            Assert.assertEquals("Nombre was not recovered correctly (" + cliente.getDireccion()+ ")", direccion, cliente.getDireccion());
+            Assert.assertNotEquals("Nombre was not recovered correctly (" + cliente.getId()+ ")", 0, cliente.getId());
+        }
     }
 
 }
